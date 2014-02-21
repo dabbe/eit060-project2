559c7ae27f8f37dd8c42c1348bf400ebd565bcc4
Daniel Olsson <atf10dol@student.lu.se>
2014-02-21 13:10:20 +0100

    added gson library

--- server/src/server/test.java
+++ server/src/server/test.java
@@ -1,17 +1,24 @@
 package server;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.google.gson.Gson;
 
 public class test {
 	public static void main(String[] args) {
 		DatabaseManager dbm;
+		Gson gson = new Gson();
 		try {
 			dbm = new DatabaseManager();
-			//dbm.createTable();
-	
-//			Record record = new Record(3, "Kalle", "Nörse", "Dåktor", "Divishon", "Dataspela");
-//			dbm.updatePatientRecord(record);
-			
+			// dbm.createTable();
+
+			// Record record = new Record(3, "Kalle", "Nörse", "Dåktor",
+			// "Divishon", "Dataspela");
+			// dbm.updatePatientRecord(record);
+
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
