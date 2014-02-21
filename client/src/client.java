cb439165ba569a4be4135fd5f17949193a7fed1d
Evozer <dat11msi@student.lu.se>
2014-02-21 13:27:26 +0100

    New cert for Alfred

--- client/src/client.java
+++ client/src/client.java
@@ -1,17 +1,3 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.security.KeyStore;
-
-import javax.net.ssl.KeyManagerFactory;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.SSLSocket;
-import javax.net.ssl.SSLSocketFactory;
-import javax.net.ssl.TrustManagerFactory;
-import javax.security.cert.X509Certificate;
 
 /*
  * This example shows how to set up a key manager to perform client
