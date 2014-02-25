37c8a7daf3b74ba60416d7b0afdda5bdfe67537d
Daniel Olsson <atf10dol@student.lu.se>
2014-02-25 14:51:17 +0100

    added identity

---
+++ server/src/resources/Identity.java
@@ -0,0 +1,21 @@
+package resources;
+
+public class Identity {
+
+	private String CN;
+	private String OU;
+	
+	public String getCN() {
+		return CN;
+	}
+	public void setCN(String cN) {
+		CN = cN;
+	}
+	public String getOU() {
+		return OU;
+	}
+	public void setOU(String oU) {
+		OU = oU;
+	}
+	
+}
